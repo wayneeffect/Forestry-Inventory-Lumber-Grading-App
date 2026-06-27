ba7d9f2b3a85896e4a46691f7988ccb3cc83f3b7
Elden Wayne Whalen III <superyoopercomputers@protonmail.com>
2026-06-27 16:01:37 -0400

    Add KML exporter for forestry inventory mapping
    This file contains KML data for a forestry inventory and lumber grading map, including styles and placemarks for various tree species with detailed log information.

---
+++ backend/kml_exporter.py
@@ -0,0 +1,114 @@
+<?xml version="1.0" ?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+  <Document>
+    <name>Forestry Inventory &amp; Lumber Grading Map</name>
+    <Style id="style_veneer">
+      <IconStyle>
+        <color>ff00aa00</color>
+        <scale>1.2</scale>
+        <Icon>
+          <href>http://maps.google.com/mapfiles/kml/paddle/wht-blank.png</href>
+        </Icon>
+      </IconStyle>
+    </Style>
+    <Style id="style_select_better">
+      <IconStyle>
+        <color>ff00c8ff</color>
+        <scale>1.2</scale>
+        <Icon>
+          <href>http://maps.google.com/mapfiles/kml/paddle/wht-blank.png</href>
+        </Icon>
+      </IconStyle>
+    </Style>
+    <Style id="style_no._1_common">
+      <IconStyle>
+        <color>ff0055ff</color>
+        <scale>1.2</scale>
+        <Icon>
+          <href>http://maps.google.com/mapfiles/kml/paddle/wht-blank.png</href>
+        </Icon>
+      </IconStyle>
+    </Style>
+    <Style id="style_pallet">
+      <IconStyle>
+        <color>ff0000ff</color>
+        <scale>1.2</scale>
+        <Icon>
+          <href>http://maps.google.com/mapfiles/kml/paddle/wht-blank.png</href>
+        </Icon>
+      </IconStyle>
+    </Style>
+    <Placemark>
+      <name>Quercus alba (White Oak) [Veneer]</name>
+      <description>&lt;![CDATA[
+        &lt;h3&gt;Log Details: LOG-A7B8C2D1&lt;/h3&gt;
+        &lt;table border=&quot;1&quot; cellpadding=&quot;5&quot; cellspacing=&quot;0&quot; style=&quot;border-collapse: collapse; font-family: sans-serif;&quot;&gt;
+            &lt;tr&gt;&lt;strong&gt;Species:&lt;/strong&gt; Quercus alba (White Oak)&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Assigned Grade:&lt;/strong&gt; &lt;span style=&quot;color:green; font-weight:bold;&quot;&gt;Veneer&lt;/span&gt;&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Estimated DBH:&lt;/strong&gt; 22.4 inches&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Clear Faces:&lt;/strong&gt; 4/4&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Timestamp:&lt;/strong&gt; 2026-06-25 14:22:00&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Field Notes:&lt;/strong&gt; Excellent veneer grade candidate. Straight grain, no visible defects.&lt;/tr&gt;
+        &lt;/table&gt;
+    ]]&gt;</description>
+      <styleUrl>#style_veneer</styleUrl>
+      <Point>
+        <coordinates>-85.5094,46.3542,0</coordinates>
+      </Point>
+    </Placemark>
+    <Placemark>
+      <name>Acer saccharum (Sugar Maple) [Select/Better]</name>
+      <description>&lt;![CDATA[
+        &lt;h3&gt;Log Details: LOG-F3E491A0&lt;/h3&gt;
+        &lt;table border=&quot;1&quot; cellpadding=&quot;5&quot; cellspacing=&quot;0&quot; style=&quot;border-collapse: collapse; font-family: sans-serif;&quot;&gt;
+            &lt;tr&gt;&lt;strong&gt;Species:&lt;/strong&gt; Acer saccharum (Sugar Maple)&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Assigned Grade:&lt;/strong&gt; &lt;span style=&quot;color:green; font-weight:bold;&quot;&gt;Select/Better&lt;/span&gt;&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Estimated DBH:&lt;/strong&gt; 18.2 inches&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Clear Faces:&lt;/strong&gt; 3/4&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Timestamp:&lt;/strong&gt; 2026-06-26 09:15:32&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Field Notes:&lt;/strong&gt; Minor frost crack on north side.&lt;/tr&gt;
+        &lt;/table&gt;
+    ]]&gt;</description>
+      <styleUrl>#style_select_better</styleUrl>
+      <Point>
+        <coordinates>-85.5212,46.361,0</coordinates>
+      </Point>
+    </Placemark>
+    <Placemark>
+      <name>Pinus strobus (Eastern White Pine) [No. 1 Common]</name>
+      <description>&lt;![CDATA[
+        &lt;h3&gt;Log Details: LOG-B2C5D6E7&lt;/h3&gt;
+        &lt;table border=&quot;1&quot; cellpadding=&quot;5&quot; cellspacing=&quot;0&quot; style=&quot;border-collapse: collapse; font-family: sans-serif;&quot;&gt;
+            &lt;tr&gt;&lt;strong&gt;Species:&lt;/strong&gt; Pinus strobus (Eastern White Pine)&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Assigned Grade:&lt;/strong&gt; &lt;span style=&quot;color:green; font-weight:bold;&quot;&gt;No. 1 Common&lt;/span&gt;&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Estimated DBH:&lt;/strong&gt; 26.1 inches&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Clear Faces:&lt;/strong&gt; 2/4&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Timestamp:&lt;/strong&gt; 2026-06-26 11:45:10&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Field Notes:&lt;/strong&gt; Large specimen, some lower branch knots.&lt;/tr&gt;
+        &lt;/table&gt;
+    ]]&gt;</description>
+      <styleUrl>#style_no._1_common</styleUrl>
+      <Point>
+        <coordinates>-85.495,46.3489,0</coordinates>
+      </Point>
+    </Placemark>
+    <Placemark>
+      <name>Populus tremuloides (Quaking Aspen) [Pallet]</name>
+      <description>&lt;![CDATA[
+        &lt;h3&gt;Log Details: LOG-D9E8F7A6&lt;/h3&gt;
+        &lt;table border=&quot;1&quot; cellpadding=&quot;5&quot; cellspacing=&quot;0&quot; style=&quot;border-collapse: collapse; font-family: sans-serif;&quot;&gt;
+            &lt;tr&gt;&lt;strong&gt;Species:&lt;/strong&gt; Populus tremuloides (Quaking Aspen)&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Assigned Grade:&lt;/strong&gt; &lt;span style=&quot;color:green; font-weight:bold;&quot;&gt;Pallet&lt;/span&gt;&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Estimated DBH:&lt;/strong&gt; 12.8 inches&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Clear Faces:&lt;/strong&gt; 1/4&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Timestamp:&lt;/strong&gt; 2026-06-27 08:05:14&lt;/tr&gt;&lt;br/&gt;
+            &lt;tr&gt;&lt;strong&gt;Field Notes:&lt;/strong&gt; High knot density, suitable for pallet stock.&lt;/tr&gt;
+        &lt;/table&gt;
+    ]]&gt;</description>
+      <styleUrl>#style_pallet</styleUrl>
+      <Point>
+        <coordinates>-85.5015,46.3575,0</coordinates>
+      </Point>
+    </Placemark>
+  </Document>
+</kml>
